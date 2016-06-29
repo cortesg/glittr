30daafcb7e7a70a9fd1b8bdba9f3a4421fbfd616
Gino Cortes <gino.angelo.cortes@gmail.com>
2016-06-29 15:12:26 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source "http://rubygems.org"
+ruby "2.3.0"
+
+gem "sinatra"
+gem "shotgun"
+gem "activerecord"
+gem "sinatra-activerecord"
+gem "sqlite3"
+gem "rake" 
+gem "sinatra-flash"
---
+++ app.rb
@@ -0,0 +1,57 @@
+#controller file
+ 
+require "sinatra"
+require "sinatra/activerecord"
+require "sinatra/flash"
+require "./models"
+
+enable :sessions
+set :database, "sqlite3:database.sqlite3"
+
+get "/" do
+	erb :index
+end
+
+
+get "/sign-in" do
+	erb :sign_in
+end
+
+post "/sign-in" do #post hides what would display in URL
+	@user = User.where(username: params[:username]).first  #.first to get rid of array
+	if @pirate && @pirate.password == params[:password]
+		session[:user_id] = @user.id
+		flash[:notice] = "You've been signed in successfully."
+		redirect "/"
+	else
+		flash[:error] = "FAILED SIGN IN."
+		redirect "/login-failed"
+	#params.inspect #puts
+	end
+end
+
+get "/sign-up" do
+	erb :sign_up
+end
+
+post '/sign-up' do
+  User.create(
+  	username: params[:username],
+  	password: params[:password],
+  	bday: params[:bday],
+  	name: params[:name]
+  	)
+  flash[:notice] = "You have signed up."
+  redirect "/"  #/post to page with posts
+end
+
+get "/login-failed" do 
+	erb :login_failed
+	# "login failed"
+end
+
+def current_user     
+	if session[:user_id]       
+		@current_user = User.find(session[:user_id])     
+	end   
+end
